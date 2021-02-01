e83d5d953b7eda9a60ee4ae677b15181c657b5c5
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-01 09:58:05 +0000

    first commit

---
+++ src/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <header>
+      <h1>Melissa's Game</h1>
+      <h2>Catch them all!</h2>
+    </header>
+  );
+};
+
+export default Title;
